0129087e217ae0fed81d3ef0b9b8cfd85efe96a5
AartiDalvi10 <Aarti_dalvi@syntelinc.com>
2019-10-14 10:28:22 +0000

    JAVA and Maps Changes

--- UrbanDevelopmentProj/Controllers/HomeController.cs
+++ UrbanDevelopmentProj/Controllers/HomeController.cs
@@ -9,6 +9,9 @@ using UrbanDevelopmentProj.Models;
 using System.IO;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using Newtonsoft.Json;
+using System.Text;
+using locationsFind.Models;
 
 namespace UrbanDevelopmentProj.Controllers
 {
@@ -16,26 +19,31 @@ namespace UrbanDevelopmentProj.Controllers
     {
         public IActionResult Index()
         {
-            string apiUrl = "http://localhost:8083/areaDetail/1";
+            //string apiUrl = "http://localhost:8083/areaDetail/1";
 
-            Uri address = new Uri(apiUrl);
+            //Uri address = new Uri(apiUrl);
 
-            // Create the web request
-            HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
+            //// Create the web request
+            //HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
 
-            // Set type to POST
-            request.Method = "GET";
-            request.ContentType = "application/json";
-            // request.charset = "UTF - 8";
+            //// Set type to POST
+            //request.Method = "GET";
+            //request.ContentType = "application/json";
+            //// request.charset = "UTF - 8";
 
-            //using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            //{
-            //    // Get the response stream
-            //    StreamReader reader = new StreamReader(response.GetResponseStream());
+            ////using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            ////{
+            ////    // Get the response stream
+            ////    StreamReader reader = new StreamReader(response.GetResponseStream());
 
-            //    // Console application output
-            //    ViewData["Message"] = reader.ReadToEnd();
-            //}
+            ////    // Console application output
+            ////    ViewData["Message"] = reader.ReadToEnd();
+            ////}
+
+            //SendMail();
+
+
+            ViewBag.BackgroudImage = "/Images/Background.jpg";
 
             return View();
         }
@@ -50,7 +58,7 @@ namespace UrbanDevelopmentProj.Controllers
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-            SendMail();
+            
             return View();
         }
 
@@ -121,6 +129,68 @@ namespace UrbanDevelopmentProj.Controllers
             }
         }
 
+        [AcceptVerbs("Post")]
+        public ActionResult getschoolincity(string json)
+        {
+            try
+            {
+                //var result = @"{'searchObjectType':'School','location':'Airoli','locationLat':'19.159014','locationLng':'72.9985686','searchObjectDetail':[{'name':'Nilkanth Patil Vidyalaya','address':'Plot no 58, 59, Shani Mandir Road, Sector 20C, Airoli','lat':'19.1624849','lng':'72.9905583'},{'name':'Manish Chandarana','address':'Mugalsan Road, Sector 20C, Sector-19, Airoli, Navi Mumbai','lat':'19.16071699999999','lng':'72.98941099999999'},{'name':'Little Scholars','address':'Shop No.1 , Plot No - C-80 , Ekuiraa Apartment, Shani Mandir Road, Sector 20, Airoli, Navi Mumbai','lat':'19.162749','lng':'72.990694'},{'name':'Funschool Play School & Nursery','address':'2, Sri Sadguru Vamanrao Pai Marg, Sector-19, Airoli, Navi Mumbai','lat':'19.160959','lng':'72.989847'},{'name':'TILLI TONG','address':'Ashapura Park, Shho no. 8, near Andhra Bank, CHS Road, Sector-19, Airoli, Navi Mumbai','lat':'19.1589599','lng':'72.9913294'},{'name':'Little Millennium - Preschool & Daycare, Airoli, Sector 19 - Navi Mumbai','address':'Plot No. 44, Laxman Plaza, Sector-19, Airoli, Navi Mumbai','lat':'19.1597117','lng':'72.9911058'},{'name':'Mindseed Preschool & Daycare','address':'Shop No. 9/10,11, Plot No. 36, Narmada Co-op Housing, Sector19, Airoli, Navi Mumbai','lat':'19.1605827','lng':'72.99057379999999'},{'name':'Aai The Day Care Center','address':'Shop No.2, Whispering Palm, Plot No.49, Sector-19, Airoli, Navi Mumbai','lat':'19.159455','lng':'72.991052'}],'suggestion':['Need More Schools ','Survey to check kids exempted from education','More nursery play group is required']}";
+                PostParameters data =JsonConvert.DeserializeObject<PostParameters>(json);
+                string city = data.city;
+                string searchType = data.searchType;
+
+                shoolDetails schoolData = new shoolDetails();
+                string apiUrl = "http://localhost:8083/getLocationAndSuggestionDetail/?location=" + city + "&searchObject=" + searchType + "";
+                Uri address = new Uri(apiUrl);
+
+                // Create the web request
+                HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
+
+                // Set type to POST
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                // request.charset = "UTF - 8";
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    // Get the response stream
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                    // Console application output
+                    var result = reader.ReadToEnd();
+                    schoolData = JsonConvert.DeserializeObject<shoolDetails>(result);
+                }
+
+                var lstSchooList = schoolData.searchObjectDetail;
+                var lstsuggestions = schoolData.suggestion;
+
+                StringBuilder citycordinates = new StringBuilder();
+                citycordinates.Append(schoolData.locationLat);
+                citycordinates.Append(',');
+                citycordinates.Append(schoolData.locationLng);
+
+                var jsclobj = JsonConvert.SerializeObject(lstSchooList);
+                var jscitycordinates = JsonConvert.SerializeObject(citycordinates.ToString());
+                var jssuggestion = JsonConvert.SerializeObject(lstsuggestions);
+
+                var returnvalue = new
+                {
+
+                    citylonglat = jscitycordinates,
+                    schooldetails = jsclobj,
+                    suggestions= jssuggestion
+                };
+
+                return Json(returnvalue);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return Json(null);
+            }
+        }
+
+       
     }
     public class EmailDTO
     {
@@ -132,6 +202,11 @@ namespace UrbanDevelopmentProj.Controllers
         public string HtmlContent { get; set; }
         public string PlainTextContent { get; set; }
 
+    }
+    public class PostParameters
+    {
+        public string city { get; set; }
+        public string searchType { get; set; }
     }
 }
 
---
+++ UrbanDevelopmentProj/Models/schoolData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace locationsFind.Models
+{
+    public class schoolData
+    {
+        public int id { get; set; }
+        public string SchoolName { get; set; }
+        public string GeoLong { get; set; }
+        public string GeoLat { get; set; }
+
+    }
+
+    public class shoolDetails
+    {
+        public string searchObjectType { get; set; }
+        public string location { get; set; }
+        public string locationLat { get; set; }
+        public string locationLng { get; set; }
+        public List<searchObjectDetail> searchObjectDetail { get; set; }
+        public List<string> suggestion { get; set; }
+    }
+
+    public class searchObjectDetail
+    { 
+        public string name {get; set;}
+        public string address {get; set;}
+        public string lat {get; set;}
+        public string lng { get; set; }
+    }
+    public class Suggestions
+    {
+        public string value { get; set; }
+        
+    }
+}
